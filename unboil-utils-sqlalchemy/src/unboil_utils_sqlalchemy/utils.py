cfe1e3ab1d6ad19083e0f8aa6952dd57c6c78e49
eyueldk <eyueldk@gmail.com>
2025-06-28 18:29:37 +0300

    Update version to 0.1.4 in pyproject.toml, add new dependencies for enhanced functionality, and refactor Core class for improved event handling and lifespan management in FastAPI application.

---
+++ unboil-utils-sqlalchemy/src/unboil_utils_sqlalchemy/utils.py
@@ -0,0 +1,35 @@
+from typing import Any
+
+from pydantic import TypeAdapter
+from sqlalchemy import JSON, Dialect, TypeDecorator
+from sqlalchemy.dialects.postgresql import JSONB
+
+
+class PydanticModel(TypeDecorator):
+    impl = JSONB
+    cache_ok = True  # Performance hint
+
+    def __init__(self, pydantic_type: type[Any]):
+        super().__init__()
+        self.pydantic_type = pydantic_type
+        self.adapter = TypeAdapter(pydantic_type)
+
+    def load_dialect_impl(self, dialect: Dialect):
+        if dialect.name == "postgresql":
+            return dialect.type_descriptor(JSONB())
+        else:
+            return dialect.type_descriptor(JSON())
+
+    def process_bind_param(self, value: Any, dialect: Dialect):
+        if isinstance(value, self.pydantic_type):
+            return self.adapter.dump_python(value)
+        return value
+
+    def process_result_value(self, value: Any, dialect: Dialect):
+        if value is None:
+            return None
+        return self.adapter.validate_python(value)
+    
+    def __repr__(self) -> str:
+        # Used by alembic
+        return f"PydanticModel({self.pydantic_type.__name__})"
