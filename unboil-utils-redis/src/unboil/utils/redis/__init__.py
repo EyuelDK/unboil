b19793e1927fda057ab114dbed9f46b4063cdd96
eyueldk <eyueldk@gmail.com>
2025-07-17 12:29:23 +0300

    Bump version to 0.1.2 and enhance redis_cached decorator to support async functions

--- unboil-utils-redis/src/unboil/utils/redis/__init__.py
+++ unboil-utils-redis/src/unboil/utils/redis/__init__.py
@@ -1,7 +1,8 @@
 import pickle
 import functools
+import asyncio
 from redis import Redis
-from typing import Callable, Optional, TypeVar, ParamSpec
+from typing import Callable, Optional, TypeVar, ParamSpec, Awaitable, Union
 
 
 P = ParamSpec("P")
@@ -10,24 +11,45 @@ R = TypeVar("R")
 
 def redis_cached(
     client: Redis,
-    key_func: Callable[P, str], 
+    keyf: Callable[P, str], 
     expire: Optional[int] = None
-) -> Callable[[Callable[P, R]], Callable[P, R]]:
-    def decorator(func: Callable[P, R]) -> Callable[P, R]:
-        @functools.wraps(func)
-        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-            key = key_func(*args, **kwargs)
-            cached = client.get(key, )
-            if cached is not None:
-                if isinstance(cached, bytes):
-                    return pickle.loads(cached)
-                if isinstance(cached, str):
-                    encoder = client.get_encoder()
-                    return pickle.loads(encoder.encode(cached))
-            result = func(*args, **kwargs)
-            client.set(key, pickle.dumps(result), ex=expire)
-            return result
+) -> Callable[[Callable[P, Union[R, Awaitable[R]]]], Callable[P, Union[R, Awaitable[R]]]]:
 
-        return wrapper
+    def get_cached(key: str) -> R | None:
+        cached = client.get(key)
+        if cached is not None:
+            if isinstance(cached, bytes):
+                return pickle.loads(cached)
+            if isinstance(cached, str):
+                encoder = client.get_encoder()
+                return pickle.loads(encoder.encode(cached))
+        return None
+
+    def set_cached(key: str, value: R) -> None:
+        client.set(key, pickle.dumps(value), ex=expire)
+
+    def decorator(func: Callable[P, R]) -> Callable[P, Union[R, Awaitable[R]]]:
+        if asyncio.iscoroutinefunction(func):
+            @functools.wraps(func)
+            async def async_wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+                key = keyf(*args, **kwargs)
+                cached = get_cached(key)
+                if cached is not None:
+                    return cached
+                result = await func(*args, **kwargs)
+                set_cached(key, result)
+                return result
+            return async_wrapper
+        else:
+            @functools.wraps(func)
+            def sync_wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+                key = keyf(*args, **kwargs)
+                cached = get_cached(key)
+                if cached is not None:
+                    return cached
+                result = func(*args, **kwargs)
+                set_cached(key, result)
+                return result
+            return sync_wrapper
 
     return decorator
