04ec2ed7f786ad1a61fa947726ff553cb467565f
eyueldk <eyueldk@gmail.com>
2025-07-21 07:17:03 +0300

    Bump version to 0.1.9 in pyproject.toml and refactor register_task function signature in typed_task.py

--- unboil-celery/src/unboil/celery/typed_task.py
+++ unboil-celery/src/unboil/celery/typed_task.py
@@ -3,7 +3,14 @@ import inspect
 from celery import Celery, Task, shared_task
 from celery.result import AsyncResult
 from typing import (
-    TYPE_CHECKING, Generic, ParamSpec, Callable, Awaitable, Any, TypeVar, cast
+    TYPE_CHECKING,
+    Generic,
+    ParamSpec,
+    Callable,
+    Awaitable,
+    Any,
+    TypeVar,
+    cast,
 )
 
 __all__ = [
@@ -13,24 +20,30 @@ __all__ = [
 
 
 T = TypeVar("T")
-T2 = TypeVar("T2")
 P = ParamSpec("P")
 SyncOrAsyncCallable = Callable[P, T | Awaitable[T]]
 
 
 class TypedTask(Task, Generic[P]):
+
     if TYPE_CHECKING:
+
         def __call__(self, *args: P.args, **kwargs: P.kwargs): ...
         def delay(self, *args: P.args, **kwargs: P.kwargs) -> AsyncResult: ...
 
 
-def register_task(app: Celery | None = None, base: type[T2] = TypedTask, **kwargs):
-    def decorator(main: SyncOrAsyncCallable[P, T]) -> T2:
+def register_task(
+    app: Celery | None = None, **kwargs
+) -> Callable[[SyncOrAsyncCallable[P, T]], TypedTask[P]]:
+
+    def decorator(main: SyncOrAsyncCallable[P, T]) -> TypedTask[P]:
         wrapper = main
         if inspect.iscoroutinefunction(main):
+
             def wrapper(*args: P.args, **kwargs: P.kwargs) -> Any:
                 return asyncio.run(main(*args, **kwargs))
+
         task_decorator = shared_task if app is None else app.task
-        task = task_decorator(base=base, **kwargs)(wrapper)
-        return cast(T2, task)
+        return cast(TypedTask[P], task_decorator(**kwargs)(wrapper))
+
     return decorator
